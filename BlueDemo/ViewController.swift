af5e25a245ff5338d3463eae250ffc3bf921e8ec
Chensh <mail2chensh@gmail.com>
2017-02-14 21:40:54 +0800

    Initial Commit

---
+++ BlueDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BlueDemo
+//
+//  Created by Chensh on 2017/2/14.
+//  Copyright © 2017年 Chensh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
